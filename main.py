ec4182ea22a432b40b9b471ad2d7928f992fef15
bondecai <35945575+bondecai@users.noreply.github.com>
2022-02-08 16:25:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 URL = "https://musescore.com/user/4923276/scores/5516956"
 
@@ -12,7 +13,32 @@ chrome_options.add_argument('--incognito')
 chrome_options.add_argument("--window-size=1920x10800")
 chrome_options.add_argument("start-maximised")
 
-driver = webdriver.Chrome(options=chrome_options)
+# driver = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 driver.get(URL)
 
-links = driver.find_elements(By.CLASS_NAME, "_2zZ8u").get_attribute("src")
+div = driver.find_elements(By.CSS_SELECTOR, ".vAVs3")
+print(div)
+
+svgs = []
+currentPage = 1
+for webEl in div:
+	# print(f"Starting page {currentPage}")
+	svgLink = webEl.find_element(By.CLASS_NAME, "_2zZ8u").get_attribute("src")
+	svgs.append(svgLink)
+	currentPage += 1
+driver.quit()
+
+pngFlag = False
+svgFlag = False
+if "png" in svgs[0]:
+	pngFlag = True
+else:
+	svgFlag = True
+
+# PRINTS LINKS TO SVG FILES
+print("Links to svgs")
+for link in svgs:
+	print(link)
+
+# links = driver.find_elements(By.CLASS_NAME, "_2zZ8u").get_attribute("src")
