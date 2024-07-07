0a7004e34768c556b586957653bf04b7fa79ce88
Bond Leung <35945575+bondecai@users.noreply.github.com>
2024-07-07 17:28:07 -0500

    Update main.py
    - Now uses xpath to get sheet links
    - No longer need to include name of pdf

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import img2pdf
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# py main.py [musescore link] "[name of pdf]"
+# py main.py [musescore link]
 
-if not sys.argv[1] or not sys.argv[2]:
+if len(sys.argv) < 2:
     print("Missing arguments. Please follow this format:")
-    print(r'py main.py [musescore link] "[name of pdf]"')
+    print(r'py main.py [musescore link]')
     sys.exit()
 
 URL = sys.argv[1]
-name = sys.argv[2]
 
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')
@@ -28,19 +28,27 @@ chrome_options.add_argument('--incognito')
 chrome_options.add_argument("--window-size=1920x10800")
 chrome_options.add_argument("start-maximised")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.set_window_size(1920, 10800)
+
 driver.get(URL)
 
-div = driver.find_elements(By.CSS_SELECTOR, ".vAVs3")
+# Get name and page number of sheet
+name = driver.find_elements(By.XPATH, "/html/body/div[1]/div/section/aside/div[1]/h1/span")[0].get_attribute('textContent')
+pages = driver.find_elements(By.XPATH, "/html/body/div[1]/div/section/aside/div[5]/div[2]/table/tbody/tr[1]/td/div")[0].get_attribute('textContent')
 
+# Create a list of download links for each page of the sheet music
 links = []
-currentPage = 1
-for webEl in div:
-	link = webEl.find_element(By.CLASS_NAME, "_2zZ8u").get_attribute("src")
-	links.append(link)
-	currentPage += 1
+for x in range(1, int(pages)+1):
+    webElement = driver.find_elements(By.XPATH, f"/html/body/div[1]/div/section/main/div/div[3]/div/div/div[{x}]/img")
+    link = webElement[0].get_attribute('src')
+    links.append(link)
+
+# No need for Selenium anymore
 driver.quit()
 
+# Download sheets into a temporary directory
 page = 0
 tempDir = tempfile.TemporaryDirectory()
 if "png" in links[0]:
@@ -67,5 +75,5 @@ for i in range(page):
     reader = PdfReader(f'{tempDir.name}/pg{i+1}.pdf')
     writer.addpages(reader.pages)
 writer.write(f'{name}.pdf')
-print("PDF created!")
+print(f"{name}.pdf created!")
 tempDir.cleanup()
